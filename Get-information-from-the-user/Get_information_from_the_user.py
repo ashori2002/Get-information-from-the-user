9a878d8455b6c750b4e2db47f97c95ffca675d22
ashori2002 <mohammadashori2002@gmail.com>
2023-02-08 00:06:25 +0330

    convert str to int int()

--- Get-information-from-the-user/Get_information_from_the_user.py
+++ Get-information-from-the-user/Get_information_from_the_user.py
@@ -1,2 +1,10 @@
 
-input("Please enter your name: ")
+age = input("Please enter your age: ")
+
+if int(age) > 18:
+    name = input("input your name: ")
+    print(f"Welcome {name}")
+else:
+    print("shoma kodak hastid...")
+print(type(age).__name__)
+print(type(int(age)).__name__)
